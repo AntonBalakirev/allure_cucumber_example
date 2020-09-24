787a51369eb8fd1352dbd2754b22f6bbe6833727
anton_balakirev <anton_balakirev@epage.ru>
2020-09-24 20:34:55 +0300

    Revert "remove unnecessary Allure steps and dependencies" This reverts commit c0373f1a

--- src/main/java/ru/appline/framework/pages/StrahovaniePage.java
+++ src/main/java/ru/appline/framework/pages/StrahovaniePage.java
@@ -22,6 +22,7 @@ public class StrahovaniePage extends BasePage {
      *
      * @return StrahovaniePage - т.е. остаемся на этой странице
      */
+    @Step("Проверка открытия страницы StrahovaniePage")
     public StrahovaniePage checkOpenStrahovaniePage() {
         Assert.assertEquals("Заголовок отсутствует/не соответствует требуемому",
                 "Страхование путешественников", pageTitle.getText());
@@ -34,6 +35,7 @@ public class StrahovaniePage extends BasePage {
      *
      * @return TarifPage - т.е. переходим на страницу {@link ru.appline.framework.pages.TarifPage}
      */
+    @Step("Кликаем по кнопке 'Оформить онлайн'")
     public TarifPage clickBtnOformitOnline() {
         scrollToElementJs(checkoutOnlineButton);
         elementToBeClickable(checkoutOnlineButton).click();
